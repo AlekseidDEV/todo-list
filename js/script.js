7de4b6769863a17df335d5ce6b600a8562e83da1
Alex <lesha.rudiko@gmail.com>
2024-03-06 16:12:52 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,73 @@
+"use strict"
+
+const todoControl = document.querySelector('.todo-control')
+const todoList = document.querySelector('.todo-list')
+const todocompleted = document.querySelector('.todo-completed')
+const headerInput = document.querySelector('.header-input')
+
+let todoData = []
+
+const setLocalStor = (data) => {
+    const dataString = JSON.stringify(data)
+    localStorage.setItem('todo', dataString)
+}
+
+const render = () => {
+    todoList.innerHTML = ''
+    todocompleted.innerHTML = ''
+
+    todoData.forEach((item, index) => {
+        const li = document.createElement('li')
+
+        li.classList.add('todo-item')
+
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>'
+
+        if (item.completed) {
+            todocompleted.append(li)
+        } else {
+            todoList.append(li)
+        }
+
+        li.querySelector('.todo-complete').addEventListener('click', (e) => {
+            item.completed = !item.completed
+            render()
+        })
+        li.querySelector('.todo-remove').addEventListener('click', (e) => {
+            todoData.splice(index,  index === 0 ? index = 1 : index)
+            render()
+        })
+    })
+
+    setLocalStor(todoData)
+}
+
+const getDataLocalStor = () => {
+    const dataString = localStorage.getItem('todo')
+    todoData = JSON.parse(dataString) || []
+
+    render()
+}
+
+getDataLocalStor()
+
+todoControl.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (headerInput.value !== '') {
+        const newTodo = {
+            text: headerInput.value,
+            completed: false
+        }
+
+        todoData.push(newTodo)
+        headerInput.value = ''
+
+        render()
+    } else{
+        alert('заполните поле')
+    }
+})
